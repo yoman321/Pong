850bcdaf10bddd3b413337a92ad16153f7a336dc
yoman3215 <luophilip@outlook.com>
2022-03-06 18:32:37 -0500

    Made the player bars with up and down movement

--- bar.py
+++ bar.py
@@ -1,2 +1,9 @@
-class bar:
-    def __init__(self, x1,)
+import pygame
+
+class Bar:
+    def __init__(self, x1, y1, width, height):
+        self.x1 = x1
+        self.y1 = y1
+        self.height = height
+        self.width = width
+
--- main.py
+++ main.py
@@ -1,13 +1,71 @@
+import Bar
 import pygame
+from pygame.locals import *
+
+#create a movement dictionary
+moveDict = {
+    K_DOWN: [0, 5],
+    K_UP: [0, -5],
+}
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 600))
+screen = pygame.display.set_mode((1000, 800))
 pygame.display.update()
-running = True
 
+#create the bars
+bar1 = Rect(25, 350, 5, 100)
+bar2 = Rect(970, 350, 5, 100)
+
+#create the clock 
+clock = pygame.time.Clock()
+
+#boolean stateemtn to check if keys are still pressed
+keyUp = False
+keyDown = False
+keyW = False
+keyS = False
+
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_DOWN:
+                keyDown = True
+            if event.key == K_UP:
+                keyUp = True
+            if event.key == K_w:
+                keyW = True
+            if event.key == K_s:
+                keyS = True
+        if event.type == pygame.KEYUP:
+            if event.key == K_DOWN:
+                keyDown = False
+            if event.key == K_UP:
+                keyUp = False
+            if event.key == K_w:
+                keyW = False
+            if event.key == K_s:
+                keyS = False
+            
+    #Move bar while key is pressed
+    if keyDown:
+        bar1.move_ip(moveDict[K_DOWN])
+    if keyUp:
+        bar1.move_ip(moveDict[K_UP])
+    if keyW:
+        bar2.move_ip(moveDict[K_UP])
+    if keyS:
+        bar2.move_ip(moveDict[K_DOWN])
+
+    #update the screen
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (0, 255, 0), bar2)
+    pygame.draw.rect(screen, (255, 0, 0), bar1)
+    pygame.display.update()
+
+    #tick the clock to check next update
+    clock.tick(60)
 
 pygame.quit() #quit pygame
