91a7582d8e8da809bab1f3876a84a6ac24f63184
yoman3215 <luophilip@outlook.com>
2022-03-30 22:15:19 -0400

    Fixed more bar-ball collision bugs

--- ball.py
+++ ball.py
@@ -3,7 +3,7 @@ from Border import Border
 import math
 
 class Ball:
-    def __init__(self, x, y, radius, moveSpeed, moveX, moveY, angle):
+    def __init__(self, x, y, radius, moveSpeed, moveX, moveY, angle, firstCollision):
         self.x = x
         self.y = y
         self.radius = radius
@@ -11,6 +11,7 @@ class Ball:
         self.moveX = moveX
         self.moveY = moveY
         self.angle = angle
+        self.firstCollision = firstCollision
     
     #check distance between either bar
     def distBar(self, bar):
@@ -21,46 +22,59 @@ class Ball:
         else:
             extremity = self.x + self.radius
         
-        if bar.player == "left" and extremity <= bar.xTop + bar.width:
+        print("extremity {} {} {} {}".format(extremity, self.x, self.y, bar.player))#test
+        if bar.player == "left" and extremity <= (bar.xTop + bar.width):
+            print("here bar left {} {}".format(bar.yTop, bar.yBot))#test
             if self.y >= bar.yTop and self.y <= bar.yBot:
-                self.moveSpeed = 5
+                # print("here bar left")#test
+                self.moveSpeed = self.moveSpeed * -1
                 if self.y >= bar.yTop and self.y < bar.yTop + 25:
-                    self.angle = 290
+                    self.angle = 315
                 elif self.y >= bar.yTop + 25 and self.y < bar.yTop + 50:
-                    self.angle = 325
+                    self.angle = 335
                 
                 elif self.y >= bar.yTop + 50 and self.y < bar.yTop + 75:
-                    self.angle = 70
+                    self.angle = 50
                     
                 elif self.y >= bar.yTop + 75 and self.y <= bar.yBot:
-                    self.angle = 35
-            
+                    self.angle = 25
+
                 #change movement for x and y
+                if self.firstCollision:
+                    self.moveSpeed = 8
+                    self.firstCollision = False
+
                 self.moveX = math.cos(math.radians(self.angle)) * self.moveSpeed
                 self.moveY = math.sin(math.radians(self.angle)) * self.moveSpeed
 
         elif bar.player == "right" and extremity >= bar.xTop:
             if self.y >= bar.yTop and self.y <= bar.yBot:
-                self.moveSpeed = -5 #change movement direction
+                self.moveSpeed = self.moveSpeed * -1 #change movement direction
+                # print("here bar right")#test
                 if self.y >= bar.yTop and self.y < bar.yTop + 25:
-                    self.angle = 35
+                    self.angle = 25
                 elif self.y >= bar.yTop + 25 and self.y < bar.yTop + 50:
-                    self.angle = 70
+                    self.angle = 50
                 
                 elif self.y >= bar.yTop + 50 and self.y < bar.yTop + 75:
-                    self.angle = 325
+                    self.angle = 335
                     
                 elif self.y >= bar.yTop + 75 and self.y <= bar.yBot:
-                    self.angle = 290
+                    self.angle = 315
             
                 #change movement for x and y
+                if self.firstCollision:
+                    print("right self collision")
+                    self.moveSpeed = -8
+                    self.firstCollision = False
+
                 self.moveX = math.cos(math.radians(self.angle)) * self.moveSpeed
                 self.moveY = math.sin(math.radians(self.angle)) * self.moveSpeed
 
     def distBorder(self, border):
         extremity = 0
         if (border.name == "top"):
-            extremity = self.y - self.radius
+            extremity = self.y - self.radius + 1
         else:
             extremity = self.y + self.radius
         
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ screen = pygame.display.set_mode((1000, 800))
 pygame.display.update()
 
 #create the bars
-bar1 = Bar(25, 350, 25, 450, 5, 100, "left")
-bar2 = Bar(970, 350, 970, 450, 5, 100, "right")
+bar1 = Bar(25, 350, 25, 450, 5, 100, "left", 8)
+bar2 = Bar(970, 350, 970, 450, 5, 100, "right", 8)
 rectBar1 = Rect(bar1.xTop, bar1.yTop, bar1.width, bar1.height)
 rectBar2 = Rect(bar2.xTop, bar2.yTop, bar1.width, bar2.height)
 
@@ -21,10 +21,11 @@ rectTopBorder = Rect(topBorder.xLeft, topBorder.yLeft, topBorder.width, topBorde
 rectBotBorder = Rect(botBorder.xLeft, botBorder.yLeft, botBorder.width, botBorder.height)
 
 #create values for ball
-ball = Ball(500, 400, 13, -5, -5, 0, 180)
+ball = Ball(500, 400, 13, -5, -5, 0, 180, True)
 
 #create the clock 
 clock = pygame.time.Clock()
+tickCheck = 0
 
 #boolean stateemtn to check if keys are still pressed
 keyUp = False
@@ -59,38 +60,38 @@ while  running:
             
     #Move bar while key is pressed-
     if keyS:
-        bar1.yTop += 5
-        bar2.yBot += 5
-        rectBar1.move_ip(0, 5)
+        bar1.yTop += bar1.moveSpeed
+        bar1.yBot += bar1.moveSpeed
+        rectBar1.move_ip(0, bar1.moveSpeed)
     if keyW:
-        bar1.yTop += -5
-        bar2.yBot += -5
-        rectBar1.move_ip(0, -5)
+        bar1.yTop += -bar1.moveSpeed
+        bar1.yBot += -bar1.moveSpeed
+        rectBar1.move_ip(0, -bar1.moveSpeed)
     if keyUp:
-        bar2.yTop += -5
-        bar2.yBot += -5
-        rectBar2.move_ip(0, -5)
+        bar2.yTop += -bar2.moveSpeed
+        bar2.yBot += -bar2.moveSpeed
+        rectBar2.move_ip(0, -bar2.moveSpeed)
     if keyDown:
-        bar2.yTop += 5
-        bar2.yBot += 5
-        rectBar2.move_ip(0, 5)
+        bar2.yTop += bar2.moveSpeed
+        bar2.yBot += bar2.moveSpeed
+        rectBar2.move_ip(0, bar2.moveSpeed)
 
    #change ball direction if ever it hits something
-    ball.distBar(bar1)
-    ball.distBar(bar2)
     ball.distBorder(topBorder)
     ball.distBorder(botBorder)
+    ball.distBar(bar1)
+    ball.distBar(bar2)
     ball.x += ball.moveX
     ball.y += ball.moveY
 
     #update the screen
     screen.fill((0, 0, 0)) #reset screen
     pygame.draw.line(screen, (255, 1, 1), (500, 0), (500, 800), 1) #draw middle line
-    pygame.draw.circle(screen, (0, 0, 255), (ball.x, ball.y), ball.radius, 0) #draw ball
     pygame.draw.rect(screen, (0, 255, 0), rectBar2) #draw player bars
     pygame.draw.rect(screen, (255, 0, 0), rectBar1)
     pygame.draw.rect(screen, (0, 255, 255), rectTopBorder)
     pygame.draw.rect(screen, (0, 255, 255), rectBotBorder)
+    pygame.draw.circle(screen, (0, 0, 255), (ball.x, ball.y), ball.radius, 0) #draw ball
     pygame.display.update()
 
     #tick the clock to check next update
